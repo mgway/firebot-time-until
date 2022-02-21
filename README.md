d73fd9219c0d6117ca02957a704d06f36c44146e
Matthew Galloway <mgalloway@silverbottle.net>
2022-02-21 10:57:39 -0600

    Initial

--- README.md
+++ README.md
@@ -1,10 +1,32 @@
-# Starter Firebot Custom Script in Typescript
+# Firebot Script: Time Until
 
-### Setup
-1. Create a new repo based off this template (Click "Use this Template" above) or simply fork it
-2. `npm install`
+A Firebot custom script to show in chat how long until a given time
+
+### End-user install
+1. Download `TimeUntil.js` from the `dist/` directory or from GitHub releases section
+2. In Firebot, make a new !command
+3. Set the trigger, cooldown, aliases, etc to whatever you'd like
+4. Under Base Effects, add new effect, then "Run Custom Script"
+   - If you don't see this option, ensure that "Custom Scripts" is enabled in Settings > Scripts
+5. Click on the "scripts folder" link in this dialog
+6. Copy `TimeUntil.js` into this folder
+7. Click the refresh icon next to the Pick One dropdown
+8. Select `TimeUntil.js`
+9. Enter the target date, message and who to send the message as
+10. Click Add, then Save
+11. Test it out
+
+
+**A note on %rDays vs %days:**
+
+The non-prefixed components round up while the r-prefixed components do not. The difference is subtle, so here is an example: 
+If today is May 5th and your birthday is on the 10th, you'd say "There are 5 days until my birthday" or, if you're a very precise person,
+"My birthday is in 4 days, 8 hours". If your command looks like the former, use %days; with the latter, use %rDays/%rMinutes. 
 
 ### Building
+
+Run `npm install`
+
 Dev:
 1. `npm run build:dev`
 - Automatically copies the compiled .js to Firebot's scripts folder.
@@ -12,7 +34,3 @@ Dev:
 Release:
 1. `npm run build`
 - Copy .js from `/dist`
-
-### Note
-- Keep the script definition object (that contains the `run`, `getScriptManifest`, and `getDefaultParameters` funcs) in the `index.ts` file as it's important those function names don't get minimized.
-- Edit the `"scriptOutputName"` property in `package.json` to change the filename of the outputted script.
--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,91 @@
-import { Firebot } from "firebot-custom-scripts-types";
+import {Firebot} from "firebot-custom-scripts-types";
 
 interface Params {
-  message: string;
+    date: string;
+    message: string;
+    sendAs: [];
 }
 
 const script: Firebot.CustomScript<Params> = {
-  getScriptManifest: () => {
-    return {
-      name: "Starter Custom Script",
-      description: "A starter custom script for build",
-      author: "SomeDev",
-      version: "1.0",
-      firebotVersion: "5",
-    };
-  },
-  getDefaultParameters: () => {
-    return {
-      message: {
-        type: "string",
-        default: "Hello World!",
-        description: "Message",
-        secondaryDescription: "Enter a message here",
-      },
-    };
-  },
-  run: (runRequest) => {
-    const { logger } = runRequest.modules;
-    logger.info(runRequest.parameters.message);
-  },
+    getScriptManifest: () => {
+        return {
+            name: "Time Until",
+            description: "Send a message about the time until an event like a game release or a birthday",
+            author: "oh_mg",
+            version: "1.0",
+            firebotVersion: "5",
+        };
+    },
+    getDefaultParameters: () => {
+        return {
+            date: {
+                type: "string",
+                default: (new Date).toISOString(),
+                description: "Target Date",
+                secondaryDescription: "Date must be formatted like '01 Jan 2022 00:00:00 GMT' or '2019-01-01T00:00:00.000Z'",
+            },
+            message: {
+                type: "string",
+                useTextArea: true,
+                default: "%days days until Elden Ring",
+                description: "Message to send to chat",
+                secondaryDescription: "Variable are %days, %hours, %minutes, %seconds, %rDays, %rHours %rMinutes. " +
+                    "The 'r' prefixed variables should be used when multiple time components are used, e.g. '%rDays days %rHours hours until Elden Ring', " +
+                    "while the non-prefixed variables should be used when the duration is by itself, e.g. '%days until Elden Ring'",
+            },
+            sendAs: {
+                type: "enum",
+                default: "Streamer",
+                description: "Send the chat message as",
+                secondaryDescription: "'Bot' has no effect if no bot user is set up",
+                options: ["Streamer", "Bot"],
+            },
+        };
+    },
+    run: (runRequest) => {
+        const {logger} = runRequest.modules;
+
+        logger.info(runRequest.parameters.message);
+        logger.info("" + Date.parse(runRequest.parameters.date));
+        logger.info("" + runRequest.parameters.sendAs);
+
+        const now = new Date()
+        const targetDate = Date.parse(runRequest.parameters.date)
+        const sendAs = runRequest.parameters.sendAs
+        const message = runRequest.parameters.message
+
+        // @ts-ignore
+        const totalSeconds = (targetDate - now) / 1000
+        const totalMinutes = Math.ceil(totalSeconds / 60)
+        const totalHours = Math.ceil(totalSeconds / (60*60))
+        const totalDays = Math.ceil(totalSeconds / (60*60*24))
+
+        const days = Math.floor(totalSeconds / (60*60*24))
+        const hours = Math.floor(totalSeconds % (60*60*24) / 3600)
+        const minutes = Math.floor(totalSeconds % (60*60*24) % 3600 / 60)
+
+        const formattedMessage = message
+            .replace('%rDays', `${days}`)
+            .replace('%rHours', `${hours}`)
+            .replace('%rMinutes', `${minutes}`)
+            .replace('%seconds', `${totalSeconds}`)
+            .replace('%minutes', `${totalMinutes}`)
+            .replace('%hours', `${totalHours}`)
+            .replace('%days', `${totalDays}`)
+
+        return new Promise((resolve, reject) => {
+            resolve({
+                success: true,
+                effects: [
+                    {
+                        type: "firebot:chat",
+                        message: formattedMessage,
+                        chatter: sendAs,
+                    }
+                ]
+            })
+        })
+    },
 };
 
 export default script;
